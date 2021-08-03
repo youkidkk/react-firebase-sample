3650994aa4f652bca0994aaec7974f6c0e733331
youkidkk <youkidkk80@gmail.com>
2021-08-03 22:24:31 +0900

    AppBar にサインアウトボタンを追加

--- src/components/app-bar.js
+++ src/components/app-bar.js
@@ -1,24 +1,44 @@
 import {
   AppBar as MuiAppBar,
+  IconButton,
   makeStyles,
   Toolbar,
   Typography,
 } from "@material-ui/core";
+import { ExitToApp } from "@material-ui/icons";
+import { useContext } from "react";
+import { app } from "../firebase-app";
+import { AuthContext } from "../auth/AuthProvider";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     maxHeight: "64px",
     marginBottom: "1.0rem",
   },
+  grow: {
+    flexGrow: 1,
+  },
 }));
 
 export default function AppBar(props) {
   const classes = useStyles();
 
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <MuiAppBar position="sticky" className={classes.appBar}>
       <Toolbar>
         <Typography variant="h6">{props.appTitle}</Typography>
+        <div className={classes.grow} />
+        {currentUser && (
+          <IconButton
+            edge="end"
+            color="inherit"
+            onClick={() => app.auth().signOut()}
+          >
+            <ExitToApp />
+          </IconButton>
+        )}
       </Toolbar>
     </MuiAppBar>
   );
