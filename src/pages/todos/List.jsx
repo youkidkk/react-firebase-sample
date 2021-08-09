4a7c20f876456e3be6e4acdd4114240fb27ebc61
youkidkk <youkidkk80@gmail.com>
2021-08-09 21:06:29 +0900

    Card による外枠を追加

--- src/pages/todos/List.jsx
+++ src/pages/todos/List.jsx
@@ -1,10 +1,17 @@
-import { Box, Button } from "@material-ui/core";
+import { Box, Button, Card, makeStyles } from "@material-ui/core";
 import { DataGrid } from "@material-ui/data-grid";
 import { AuthContext } from "auth/AuthProvider";
 import ContentsTitle from "components/ContentsTitle";
 import { getTodos } from "firebase-db";
 import { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    width: "100%",
+    padding: "1rem",
+  },
+}));
 
 const columns = [
   {
@@ -26,6 +33,8 @@ const getTodosAsync = async (uid, setTodos) => {
 };
 
 const List = () => {
+  const classes = useStyles();
+
   const { currentUser } = useContext(AuthContext);
   const uid = currentUser.uid;
   const history = useHistory();
@@ -42,23 +51,25 @@ const List = () => {
 
   return (
     <>
-      <ContentsTitle title="Todo一覧" />
-      <Box mb={2} display="flex" justifyContent="flex-end">
-        <Button onClick={handleCreateButtonClick}>新規作成</Button>
-      </Box>
-      <DataGrid
-        rows={todos}
-        columns={columns}
-        rowsPerPageOptions={[5, 10, 20]}
-        pageSize={10}
-        autoHeight
-        disableColumnFilter
-        disableColumnMenu
-        disableColumnSelector
-        disableDensitySelector
-        disableSelectionOnClick
-        onRowClick={handleRowClick}
-      />
+      <Card className={classes.card}>
+        <ContentsTitle title="Todo一覧" />
+        <Box mb={2} display="flex" justifyContent="flex-end">
+          <Button onClick={handleCreateButtonClick}>新規作成</Button>
+        </Box>
+        <DataGrid
+          rows={todos}
+          columns={columns}
+          rowsPerPageOptions={[5, 10, 20]}
+          pageSize={10}
+          autoHeight
+          disableColumnFilter
+          disableColumnMenu
+          disableColumnSelector
+          disableDensitySelector
+          disableSelectionOnClick
+          onRowClick={handleRowClick}
+        />
+      </Card>
     </>
   );
 };
