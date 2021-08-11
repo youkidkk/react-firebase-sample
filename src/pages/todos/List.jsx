47600164432f369c757e405d461a376227267e6b
youkidkk <youkidkk80@gmail.com>
2021-08-11 13:06:10 +0900

    スマホ用画面表示を追加

--- src/pages/todos/List.jsx
+++ src/pages/todos/List.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, makeStyles } from "@material-ui/core";
+import { Box, Button, Card, Hidden, makeStyles } from "@material-ui/core";
 import { DataGrid } from "@material-ui/data-grid";
 import { AuthContext } from "auth/AuthProvider";
 import { DATE_FORMAT_DISPLAY, PRIORITY_DISPLAY } from "common/common-const";
@@ -9,9 +9,18 @@ import { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router";
 
 const useStyles = makeStyles((theme) => ({
-  card: {
+  frame: {
     width: "100%",
     padding: "1rem",
+  },
+  itemCard: {
+    fontSize: "1.2rem",
+    margin: 3,
+    padding: 5,
+  },
+  itemCardOverView: {
+    marginTop: 2,
+    fontSize: "1.5rem",
   },
 }));
 
@@ -70,24 +79,47 @@ const List = () => {
 
   return (
     <>
-      <Card className={classes.card}>
+      <Card className={classes.frame}>
         <ContentsTitle title="Todo一覧" />
         <Box mb={2} display="flex" justifyContent="flex-end">
           <Button onClick={handleCreateButtonClick}>新規作成</Button>
         </Box>
-        <DataGrid
-          rows={todos}
-          columns={columns}
-          rowsPerPageOptions={[5, 10, 20]}
-          pageSize={10}
-          autoHeight
-          disableColumnFilter
-          disableColumnMenu
-          disableColumnSelector
-          disableDensitySelector
-          disableSelectionOnClick
-          onRowClick={handleRowClick}
-        />
+        <Hidden lgUp>
+          {(() => {
+            return todos.map((todo) => {
+              return (
+                <Card
+                  key={todo.id}
+                  className={classes.itemCard}
+                  onClick={(event) => handleRowClick({ id: todo.id }, event)}
+                >
+                  <Box display="flex" justifyContent="space-between">
+                    <div>{todo.deadlineDisplay}</div>
+                    <div>{todo.priorityDisplay}</div>
+                  </Box>
+                  <div className={classes.itemCardOverView}>
+                    {todo.overview}
+                  </div>
+                </Card>
+              );
+            });
+          })()}
+        </Hidden>
+        <Hidden mdDown>
+          <DataGrid
+            rows={todos}
+            columns={columns}
+            rowsPerPageOptions={[5, 10, 20]}
+            pageSize={10}
+            autoHeight
+            disableColumnFilter
+            disableColumnMenu
+            disableColumnSelector
+            disableDensitySelector
+            disableSelectionOnClick
+            onRowClick={handleRowClick}
+          />
+        </Hidden>
       </Card>
     </>
   );
